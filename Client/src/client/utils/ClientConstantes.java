49a581e73d7742f2a07668d6786025db4baa40f3
Kaldoran <kaldoran@live.fr>
2014-12-10 21:07:34 +0100

    Ajout du client

---
+++ Client/src/client/utils/ClientConstantes.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client.utils;
+
+/**
+ *
+ * @author kaldoran
+ */
+public class ClientConstantes {
+    public final static Integer PORT = 10777;   
+
+}
