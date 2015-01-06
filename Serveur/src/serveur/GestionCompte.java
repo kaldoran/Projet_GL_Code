2a33d0d7f45b3744090a8df131da65240d64432f
Kaldoran <kaldoran@live.fr>
2015-01-06 23:49:43 +0100

    Verification mot de passe / Login

--- Serveur/src/serveur/GestionCompte.java
+++ Serveur/src/serveur/GestionCompte.java
@@ -16,6 +16,8 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import serveur.utils.ServeurConstantes;
 import serveur.utils.toSha1;
 
@@ -25,13 +27,16 @@ import serveur.utils.toSha1;
  */
 public class GestionCompte {
 
-    public GestionCompte() { }
-    
+    private static Pattern regex = Pattern.compile("([a-z]*)@([a-zA-Z0-9]{40}):.*");
+
+    public GestionCompte() {
+    }
+
     public boolean ajouterCompte(String login, String password, String droits) {
-        
+
         BufferedWriter cpt = null;
-        
-        if ( !isCorrect(login, password) ) {
+
+        if (!isCorrect(login, password)) {
             try {
                 cpt = new BufferedWriter(new FileWriter(ServeurConstantes.FICHIER_COMPTE, true));
                 cpt.write(login + toSha1.toSHA1(password.getBytes()) + ":" + droits);
@@ -40,25 +45,25 @@ public class GestionCompte {
             } catch (IOException ex) {
                 Logger.getLogger(GestionCompte.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+
         }
-        
+
         return true;
     }
-    
+
     public boolean supprimerCompte(String login) {
         PrintWriter pw = null;
         BufferedReader br = null;
-        
+
         String line = null;
-        
+
         try {
             File account = new File(ServeurConstantes.FICHIER_COMPTE);
             File tmp = new File(ServeurConstantes.FICHIER_COMPTE + ".tmp");
-            
+
             br = new BufferedReader(new FileReader(account));
             pw = new PrintWriter(new FileWriter(tmp));
-           
+
             while ((line = br.readLine()) != null) {
                 if (!line.trim().equals(login)) {
                     pw.println(line);
@@ -72,11 +77,11 @@ public class GestionCompte {
             if (!account.delete()) {
                 System.out.println("[Erreur] : Impossible de supprimer le fichier");
             }
-            
+
             if (!tmp.renameTo(account)) {
                 System.out.println("[Erreur] : Impossible de renommer le fichier");
             }
-            
+
         } catch (IOException ex) {
             Logger.getLogger(GestionCompte.class.getName()).log(Level.SEVERE, null, ex);
             return false;
@@ -84,24 +89,29 @@ public class GestionCompte {
 
         return true;
     }
-    
+
     public static boolean isCorrect(String login, String pass) {
         String inputScanner = null;
         String hashpass = null;
         hashpass = toSha1.toSHA1(pass.getBytes());
-        
+
         Scanner scanner = null;
         try {
-            scanner = new Scanner(new File(ServeurConstantes.FICHIER_COMPTE) );
+            scanner = new Scanner(new File(ServeurConstantes.FICHIER_COMPTE));
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ServeurAuthentification.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        while(scanner.hasNextLine()){
-            if ( (inputScanner = scanner.nextLine().trim()).startsWith(login)) 
-                return true;
+
+        while (scanner.hasNextLine()) {
+            inputScanner = scanner.nextLine().trim();
+            Matcher m = regex.matcher(inputScanner);
+            if ( m.matches() ) {
+                if (login.equalsIgnoreCase(m.group(1)) && hashpass.equalsIgnoreCase(m.group(2))) {
+                    return true;
+                }
+            }
         }
-        
+
         return false;
     }
 }
